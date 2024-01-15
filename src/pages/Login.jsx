8d43b2d704584c83218af0aa57ddb37782b95127
g charan <gcharan6366@gmail.com>
2024-01-15 13:32:49 +0530

    added a box container for signup and login section

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -39,6 +39,7 @@ const Login = () => {
 
   return (
     <div className=' flex flex-col	items-center justify-center	 text-center	h-screen	login  ' >
+    <div className='login_container'>
       <h1 className='logo'>BEING HUMAN</h1>
       <form onSubmit={handleSubmit} className="login-form">
           <input className=" phSignupLogin placeholder:italic placeholder:text-slate-50 block  w-full border border-cyan-600 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
@@ -62,6 +63,7 @@ const Login = () => {
       </form>
 
       <p>Need to Signup ? <Link to="/signup">Create Account</Link></p>
+      </div>
     </div>
   )
 }
