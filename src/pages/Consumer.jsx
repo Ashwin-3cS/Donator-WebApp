5b6591a009864a1c8a0a66bd922883668a924c9e
KaushikKC <kccreations1704@gmail.com>
2024-01-15 22:24:44 +0530

    Adjusted height and padding top

--- src/pages/Consumer.jsx
+++ src/pages/Consumer.jsx
@@ -32,8 +32,8 @@ const Consumer = () => {
   return (
     <div className='consumer'>
       <NavBar navigate={navigate}/>
-      <div >
-        <h1 className='mt-[200px]'>Consumer Page</h1>
+      <div className='h-screen pt-[60px]'>
+        <h1 className=''>Consumer Page</h1>
       </div>
       {/* Add your content for the Consumer page */}
       {/* <button onClick={goToHomePage}>Go to Home</button> */}
