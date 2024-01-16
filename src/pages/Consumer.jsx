19fbe312354b46186757a31bfc63060796eec0d1
Ashwin-3cS <flashashwin2004@gmail.com>
2024-01-16 15:17:50 +0530

    again updated consumer page

--- src/pages/Consumer.jsx
+++ src/pages/Consumer.jsx
@@ -25,11 +25,11 @@ const Consumer = () => {
     <div className='consumer'>
       <NavBar navigate={navigate}/>
       <div className='full-vh-background  pt-[60px] '>
-      <h1 className='block '>hello</h1>
-
+      <h1 className=''></h1>
+      
         <AutoImageCarousel images={images} />
         
-        <div className=' flex flex-col ml-[700px] mt-[80px] text-white text-3xl '>
+        <div className=' flex flex-col ml-[710px] mt-[130px] text-white text-3xl '>
 
           <p className='mb-[10px]'>1 . Choose Your Location</p>
           <p className='mb-[10px]'>2 . Pick Your Food</p>
@@ -37,6 +37,7 @@ const Consumer = () => {
           <p>4 . Grab Your Food</p>
         
         </div>
+      
         <h1 className='mt-[800px] text-black text-3xl text-center'>hi consumer</h1>
         <h1 className='mt-[800px]'>fine ah</h1>
         
